cc239d41ada5633d552a0152086a58040b96e6c6
Yasser <yassermomade11@gmail.com>
2026-05-04 09:39:15 +0200

    feat: add Counter and country request

---
+++ app/Http/Requests/Country/StoreCountryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Country;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCountryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->isAdmin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100', 'unique:countries,name'],
+            'iva' => ['required', 'numeric', 'min: 0'],
+            'coin' => ['required', 'string', 'max: 55'],
+        ];
+    }
+}
