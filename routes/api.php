d26184a3d62b46979794e929cd6ebaec0bd1b6c1
Yasser <yassermomade11@gmail.com>
2026-05-04 12:11:46 +0200

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CounterController;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\CountryController;
 use Illuminate\Http\Request;
@@ -33,7 +34,9 @@ Route::prefix('v1')->group(function(){
         Route::middleware('role:admin')->group(function () {
 
             Route::apiResource('countries', CountryController::class);
+            Route::apiResource('counters',  CounterController::class);
         
+           
         }); 
     });
 
