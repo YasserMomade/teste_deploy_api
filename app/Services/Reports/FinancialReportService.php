10fa0c305bcd9c8f4e93252acb48bf87d123f831
Yasser <yassermomade11@gmail.com>
2026-05-20 15:38:25 +0200

    merge conflit

--- app/Services/Reports/FinancialReportService.php
+++ app/Services/Reports/FinancialReportService.php
@@ -16,11 +16,7 @@ class FinancialReportService
             ->with([
                 'client:id,name,lastname',
                 'invoice',
-<<<<<<< HEAD
-                'responsible:id,name,user_code',
-=======
                 'responsible:id,name,lastname,user_code',
->>>>>>> 88c6fdd8e6eff657ef79fa59c15942cbc1402a9e
             ])
             ->get();
         return [
@@ -72,11 +68,7 @@ class FinancialReportService
 
             SUM(invoices.amount_paid) as total_paid,
 
-<<<<<<< HEAD
-            SUM(orders.service_type = "expresso") as total_express,
-=======
             SUM(orders.service_type = "drop off") as total_express,
->>>>>>> 88c6fdd8e6eff657ef79fa59c15942cbc1402a9e
 
             SUM(orders.service_type = "normal") as total_normal,
 
