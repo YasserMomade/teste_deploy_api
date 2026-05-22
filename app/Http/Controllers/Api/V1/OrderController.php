85ab0984167a6377e4b81ccd680a70f3c1241e8e
IanMavulule <167016821+IanMavulule@users.noreply.github.com>
2026-05-22 11:37:54 +0200

    stage_level

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -42,38 +42,21 @@ class OrderController extends Controller
     public function store(StoreOrder $request): JsonResponse
     {
         try {
-            // clients
-            $data = $request->validated();
+            // cliente
+            // $data = $request->validated();
 
-            $name = $data['name'];
-            $lastname = $data['lastname'];
+            // $name = $data['name'];
+            // $lastname = $data['lastname'];
 
-            $client = [
-                "name" => $data['name'],
-                "lastname" => $data['lastname'],
-            ];
+            // $client = [
+            //     "name" => $data['name'],
+            //     "lastname" => $data['lastname'],
+            // ];
             
-            $client = $this->clientService->createClient($client);
+            // $client = $this->clientService->createClient($client);
 
             // order
-            $orderData = [
-                "client_id" => $client->id,
-                "description" => $data['description'],
-                "tracking" => $data['tracking'],
-                "origin" => $data['origin'],
-                "destination" => $data['destination'],
-                "reception_date" => $data['reception_date'],
-                "service_type" => $data['service_type'],
-                "volume_number" => $data['volume_number'],
-                "weight" => $data['weight'],
-                "declared_weight" => $data['declared_weight'],
-                "category_id" => $data['category_id'],
-                "responsible_id" => $data['responsible_id'],
-                "invoice_id" => null,
-                "store_id" => $data['store_id'],
-            ];
-
-            $order = $this->orderService->createOrder($orderData);
+            $order = $this->orderService->createOrder($request->validated());
 
             $order->load(['category.prices']);
 
