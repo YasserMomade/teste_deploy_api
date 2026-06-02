472d538414a6ea042bb076b0cded43a8a18cc0dc
Yasser <yassermomade11@gmail.com>
2026-06-02 10:49:33 +0200

    update Exception Report Export

--- app/Exports/ExceptionReportExport.php
+++ app/Exports/ExceptionReportExport.php
@@ -3,16 +3,18 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use App\Exports\Sheets;
 
 class ExceptionReportExport implements WithMultipleSheets
 {
-    public function __construct(private readonly array $data) {}
+    public function __construct(private array $data) {}
 
     public function sheets(): array
     {
         return [
             new Sheets\ExceptionSummarySheet($this->data['summary']),
             new Sheets\ExceptionDelaysSheet($this->data['delays']),
+            new Sheets\ExceptionStalledSheet($this->data['stalled']),
             new Sheets\ExceptionQualitySheet($this->data['quality']),
         ];
     }
