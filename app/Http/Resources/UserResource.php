83037bede4251cd166c7d95c8ff116df485987e9
Yasser <yassermomade11@gmail.com>
2026-06-03 08:44:06 +0200

    add User Controller and Resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'user_code'  => $this->user_code,
+            'role'       => $this->role->value,
+            'name'       => $this->name,
+            'lastname'   => $this->lastname,
+            'phone'      => $this->phone,
+            'email'      => $this->email,
+            'counter'    => new CounterResource($this->whenLoaded('counter')),
+            'created_at' => $this->created_at?->toDateTimeString(),
+        ];
+    }
+}
+
+
+
